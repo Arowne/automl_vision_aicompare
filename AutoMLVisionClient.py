11d5285f9ee0a2c3108b181140e6a70fd8811b62
Arowne <aaron.y.boaten@gmail.com>
2021-01-06 15:19:54 +0100

    [features] Add prediction

--- AutoMLVisionClient.py
+++ AutoMLVisionClient.py
@@ -1,7 +1,8 @@
-import requests 
+import requests
+
 
 class AutoMLVisionClient():
-    
+
     def __init__(self, *args, **kwargs):
         self.api_key = kwargs['api_key']
         self.server_url = 'http://0.0.0.0:8000/v1/'
@@ -15,40 +16,43 @@ class AutoMLVisionClient():
                 'name': project_name,
                 'description': project_description
             }
-            
-            response = requests.post(self.server_url + 'automl/vision/project', data=payload, headers=self.headers)
+
+            response = requests.post(
+                self.server_url + 'automl/vision/project', data=payload, headers=self.headers)
             data = response.json()
             print(response.json())
             return data['project_id']
-    
+
     def add_project_image(self, project_id=None, file_path=None):
 
             files = {
                 'files': open(file_path, 'rb')
             }
-            
-            response = requests.post(self.server_url + 'automl/vision/image/' + project_id + '/project', files=files, headers=self.headers)
+
+            response = requests.post(self.server_url + 'automl/vision/image/' +
+                                     project_id + '/project', files=files, headers=self.headers)
             data = response.json()
             return data['image_id']
-    
+
     def add_project_tag(self, project_id=None, tag_name=None):
 
             payload = {
                 'name': tag_name,
             }
-            
-            response = requests.post(self.server_url + 'automl/vision/tag/' + project_id + '/project', data=payload, headers=self.headers)
+
+            response = requests.post(self.server_url + 'automl/vision/tag/' +
+                                     project_id + '/project', data=payload, headers=self.headers)
             data = response.json()
 
             return data['tag_id']
 
     def add_image_tag(self, image_id=None, tag_id=None):
 
-            response = requests.get(self.server_url + 'automl/vision/tag/' + tag_id + '/image/' + image_id, headers=self.headers)
+            response = requests.get(self.server_url + 'automl/vision/tag/' +
+                                    tag_id + '/image/' + image_id, headers=self.headers)
             data = response.json()
 
             return data['response']
-
 
     def train_project(self, project_id=None, training_name=None, training_time=None, provider=None):
 
@@ -57,9 +61,26 @@ class AutoMLVisionClient():
                 "training_time": training_time,
                 "provider": provider
             }
-            
-            response = requests.post(self.server_url + 'automl/vision/train/' + project_id + '/project', data=payload, headers=self.headers)
+
+            response = requests.post(self.server_url + 'automl/vision/train/' +
+                                     project_id + '/project', data=payload, headers=self.headers)
             data = response.json()
             print(data)
 
             return data['train_id']
+
+    def predict(self, provider=None, image_path=None, train_id=None, project_id=None):
+
+            files = {
+                'files': open(image_path, 'rb')
+            }
+
+
+            payload = {
+                "provider": provider
+            }
+
+            response = requests.post(self.server_url + 'automl/vision/prediction/project/' +
+                                     project_id  +'/train/' + train_id, data=payload, files=files, headers=self.headers)
+            data = response.json()
+            return data['results'][0]['predictions']
--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import glob
 from AutoMLVisionClient import AutoMLVisionClient
 
 
-
 def train_project():
     
-    # Init class
-    client = AutoMLVisionClient(api_key='api_key')
-    counter = 0
+    # # Init class
+    client = AutoMLVisionClient(api_key='')
 
     print('Project creation...')
     project_id = client.create_project(project_name='Floor plans classifier', project_description='Automaticaly count number of rooms')
@@ -16,29 +14,34 @@ def train_project():
 
     # Add tag
     print('Tag creation ...')
+    three_rooms_id = client.add_project_tag(project_id=project_id, tag_name='3')
     four_rooms_id = client.add_project_tag(project_id=project_id, tag_name='4')
     five_rooms_id = client.add_project_tag(project_id=project_id, tag_name='5')
-    six_rooms_id = client.add_project_tag(project_id=project_id, tag_name='6')
     
 
-    tags = [four_rooms_id, five_rooms_id, six_rooms_id]
+    tags = [three_rooms_id, four_rooms_id, five_rooms_id]
     
     print('Image importation ...')
     # Add upload and add tag image
     for index in range(3):
         current_folder = "floor_plan_dataset/" + str(index) +"/*"
-
+        counter = 0
         for img in glob.glob(current_folder):
             counter += 1
-            print(str(counter) + '/500', end="\r")
+            if counter > 100:
+                break
+            print(str(counter*(index+1)) + '/300', end="\r")
             image_id = client.add_project_image(project_id=project_id, file_path=img)
             response = client.add_image_tag(image_id=image_id, tag_id=tags[index])
             print(response)
 
     print('Launch train ...')
     print(project_id)
-    response = client.train_project(project_id=project_id, training_name='Google train', training_time=1000, provider='google_cloud')
+    train_id = client.train_project(project_id=project_id, training_name='Room classification google train', training_time=8000, provider='google_cloud')
     print(response)
+
+    prediction = client.predict(provider="google_cloud", project_id=project_id, train_id=train_id, image_path="floor_plan_dataset/0/Cat9_9.jpg", )
+    print(prediction)
 
 if __name__ == "__main__":
     train_project()
